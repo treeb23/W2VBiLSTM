08bc6ea06b9ba5b132a4509275ed60ecfa63239f
treeb23 <105105490+treeb23@users.noreply.github.com>
2023-04-09 04:15:27 +0900

    Update W2VBiLSTM.py

--- W2VBiLSTM/W2VBiLSTM.py
+++ W2VBiLSTM/W2VBiLSTM.py
@@ -22,15 +22,24 @@ from transformers import Wav2Vec2Processor, Wav2Vec2Model
 user = 2 #2クラスにわける
 lr = 0.0001 #learning rate 学習の重みの変化
 v_size = 768 #wave2vecから出力されるベクトルのサイズ
-epoch =30 #学習回数
-bs = 190000 #データの最大サイズ
+epoch = 20 #学習回数
+bs = 156530 #データの最大サイズ
 hiddensize =128 #ネットワークの中間層のノード数
 dropout=0.3 #出力層に出力しない割合　過学習を防ぐため
 layer=1 #中間層の層の数
 filenames=['jpn','us'] #ファイルの名前の共通部分
 file_nums=[130,55] #ファイルの数
 
-def setup(): # ファイルパスの設定
+
+def set_param(learningrate=0.0001,epochnum=20,bsize=156530,filenums=[130,55]):
+    global lr,epoch,bs,file_nums
+    lr=learningrate
+    epoch=epochnum
+    bs=bsize
+    file_nums=filenums
+    
+
+def setup(): # ランダムシード、Wav2Vecモデル、ファイルパスの設定
     np.random.seed(1)
     torch.cuda.manual_seed_all(1)
     torch.cuda.manual_seed(1)
@@ -54,13 +63,105 @@ def setup(): # ファイルパスの設定
     print(f"[ファイルパスf_pathを'{f_path}'に設定]")
     t_path=f_path
 
+    
+def trainingmodel(model_name,traindatapath):
+    global t_path
+    t_path=traindatapath
+    save_model(model_name)
+    
+
+class ACDataset(Dataset):
+    def __init__(self):
+        self.data_num = 0 #全体のデータ数を保存する変数
+        self.userindex = torch.Tensor(user)
+        for USER in range(user): #クラス分類の数回るfor文
+            flag=0 #一番最初のデータを処理する用のフラッグ
+            w=0
+            wav_paths=[]
+            for i in range(file_nums[USER]): #実際に入力データのパスを格納する
+                wav_path=glob.glob(f'{f_path}/data/短文音声/training/{t_path}/{filenames[USER]}_{i+1}.wav')
+                wav_paths.append(wav_path)
+            for x in wav_paths: #実際にデータを読み込んで正解ラベルを生成する
+                _,data_x = scipy.io.wavfile.read(x[0]) #wavファイルを読み込む
+                data_Xc=np.array(data_x,dtype=float) #wavファイルをnumpy型に変換
+                data_X=torch.from_numpy(data_Xc) #wavファイルをテンソル型に変換
+                i = data_X.shape[0] #データの長さを取得
+                if USER == 0 and flag == 0: #最初のデータを処理する
+                    if i%bs != 0: #現在のデータの長さがbsで割りきれないなら処理
+                        amari = i%bs 
+                        amari = bs-amari #bsと現在のデータの長さの差を取得
+                        zeros = torch.zeros(amari,dtype=torch.float64) #amariの数だけ0が入った配列を作る
+                        self.label = torch.Tensor() #正解ラベル格納用
+                        data_X = torch.cat([zeros,data_X],axis=0) #zerosと結合してbsに長さを合わせる
+                        self.data = data_X
+                        self.label = torch.Tensor()
+                        i = amari+i
+                        flag=1
+                    else: 
+                        self.data = data_X;
+                        self.label = torch.Tensor()
+                        i = data_X.shape[0] 
+                        flag=1
+                else:
+                    if i%bs != 0:
+                        amari = i%bs
+                        amari = bs-amari
+                        zeros = torch.zeros(amari,dtype=torch.float64)
+                        data_X = torch.cat([zeros,data_X],axis=0)
+                        self.data = torch.cat([self.data,data_X],axis=0)
+                        i = amari+i
+                        #print(i)
+                    else:
+                        self.data = torch.cat([self.data,data_X],axis=0)
+                        i = data_X.shape[0]
+                w+=1 
+                s = torch.zeros(i,user) #正解ラベルを生成
+                s[0:i,USER]=1 #指定した行のラベルを1にする
+                self.label=torch.cat([self.label,s],dim=0) #全体のラベルと結合
+                self.data_num = self.data_num + i #データの個数を数える
+
+                self.userindex[USER] = len(self.data)
+
+    def __len__(self):
+        return self.data_num
+
+    def __getitem__(self, idx):
+        out_data = self.data[idx]
+        out_label =  self.label[idx]
+        out_seqlen = self.seqlen[idx]
+        
+        return out_data, out_label,out_seqlen
+    def getindex(self,USER):
+        
+        return self.userindex[USER]
+
+
+#ユーザーの数   
+alldata= ACDataset()
+train_data = ACDataset() 
+#print(alldata.getindex(0).item())
+for i in range(user): #ACDatasetで作成したデータを読み込んで変数を格納
+    if(i==0):
+        train_data.data = alldata.data[0:int((alldata.getindex(0).item()))]
+        train_data.label = alldata.label[0:int((alldata.getindex(0).item()))]
+        
+    else:
+        train_data.data = torch.cat([train_data.data,alldata.data[int(alldata.getindex(i-1).item()):(int(alldata.getindex(i).item()))]])
+        train_data.label = torch.cat([train_data.label,alldata.label[int(alldata.getindex(i-1).item()):(int(alldata.getindex(i).item()))]])
+
+#import pdb; pdb.set_trace()
+train_dataset = torch.utils.data.TensorDataset(train_data.data,train_data.label) #学習データと正解ラベルを一つにする
+train_loader = DataLoader(train_dataset, batch_size=bs,
+                          shuffle=False, num_workers=0) #bsで分割
+
+    
 
 class SequenceTaggingNet(nn.Module):
     def __init__(self,
                  input_dim=v_size, #入力層のノード数
                  hidden_size=hiddensize, #中間層のノード数
                  num_layers=layer, #中間層の層の数
-                 dropout=dropout): #どろっぷあうと率
+                 dropout=dropout): #ドロップアウト率
         super().__init__()
         self.lstm = nn.LSTM(input_dim,hidden_size, num_layers,
                             dropout=dropout,batch_first=True,bidirectional=True) #LSTMの設定
@@ -109,7 +210,37 @@ def eval_net(net, train_loader, device): #評価用関数
     acc = correct / uncleSum #正解率
     return acc,loss,tmp
 
+  
+def save_model(model_name):
+    device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
+    loss_f = nn.CrossEntropyLoss()
+    ALL_lost=0
+    net_test = SequenceTaggingNet()
+    net_test.to(device)
+    opt = optim.Adam(net_test.parameters(),lr)
+    losses = []
+    flag_net = 0
+    for t in tqdm(range(2)): #40
+        net_test.train()
+        for (a,b) in train_loader:
+            b = b[0]
+            b = b.view(1,user)
+            x = a.to(device)
+            y = b.to(device)
+            opt.zero_grad()
+            y_pred = net_test(x)
+            y = torch.argmax(y,dim=1)
+            loss = loss_f(y_pred, y.long())
+            losses.append(loss.item())
+            loss.backward()
+            opt.step()
+    train_acc,_ ,_2= eval_net(net_test, train_loader, device)
+    print(train_acc,mean(losses))
+    
+    model_path = f"{f_path}/code/save_model/{model_name}.pth" # モデル保存先パス
+    torch.save(net_test.state_dict(), model_path)
 
+    
 def load_model(model_name):
     global device,loss_f,ALL_lost,net_test,opt,model_path
     device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
@@ -235,3 +366,18 @@ def test(model_name,maxcount,testdatapath): #maxcountはJPのファイル数
         count=count+1
         if count==maxcount:
             print("ここからUS")
+
+            
+def search_bs(wavepath): #ディレクトリ内の.wavの最大bsを出力
+    wav_path=glob.glob(f'{f_path}/data/短文音声/test/{wavepath}/*.wav')
+    print(wav_path)
+    flag = 0
+    for x in wav_path:
+        _,data_x = scipy.io.wavfile.read(x)
+        if flag == 0:
+            a = len(data_x)
+            flag =1
+        elif len(data_x)>a:
+            a = len(data_x)
+    print(a)
+    
